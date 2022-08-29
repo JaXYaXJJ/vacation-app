e8ed288d646114649876ed4c204dc56b6f387535
JaXYaXJJ <ja-ya@mail.ru>
2022-08-29 21:59:59 +0300

    vacation-app

---
+++ src/components/Order.js
@@ -0,0 +1,15 @@
+function Order() {
+    return ( 
+        <nav className="navbar bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-white" href="#">
+        <i class="bi bi-airplane-fill mx-2"></i>
+          Nordic Vacation
+        </a>
+        <a class="nav-link text-white" href="#">Order Now</a>
+      </div>
+    </nav>
+     );
+}
+
+export default Order;
